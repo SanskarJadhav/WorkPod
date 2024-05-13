73dac9486c67565305d5b61a586d4e729efb7889
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2024-05-13 16:11:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -85,7 +85,7 @@ def main():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://plus.unsplash.com/premium_photo-1671751033693-8b777c36b236?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+             background-image: url("https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
              background-attachment: fixed;
              background-size: cover
          }}
