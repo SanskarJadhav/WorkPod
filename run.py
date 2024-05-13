187eca0a17712ff6fe689121c1d46101bd8dd4c6
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2024-05-13 12:39:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -154,9 +154,9 @@ def main():
             st.sidebar.header(":grey-background[Project Members]")
             project_users = get_users_by_project_id(project_id)
             for user in project_users:
-                multi = '''f"Username: {user[1]}",  
-                f"Email: {user[2]}"'''
-                st.sidebar.markdown(multi)
+                multi = '''Username: {user[1]}  
+                Email: {user[2]}'''
+                st.sidebar.markdown(f"{multi}")
                 Email: {user[2]}")
                 if user[4] is not None:
                     # Display uploaded image
