ae3f7bfea52a7240256ca8680a837a08df2388cd
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2024-05-14 14:48:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -248,7 +248,7 @@ def main():
                 if line.strip().startswith("1."):
                     save_lines = True
                 if save_lines:
-                    cleaned_line = line.strip("*").strip()
+                    cleaned_line = line.lstrip("*")
                     filtered_lines.append(cleaned_line)
             tasks = filtered_lines
             st.session_state.messages.append(message)
