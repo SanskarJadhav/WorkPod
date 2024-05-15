694ee95e531c81d82bfc1aece6d029c16f7940a5
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2024-05-15 12:22:48 +0530

    Update run.py

--- run.py
+++ run.py
@@ -423,7 +423,7 @@ def main():
                 background-color: green;
                 border-radius: 15px;
                 color: white;
-                border: 2px solid white;
+                border: 2px solid #0f0f0f;
             }""",
         ):
             happy_clicked = st.button("Happy", key="happy")
